579dc31ae3b88a0b85e32f9a54fc4258cc14b510
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:12:41 +0200

    added hw 9

---
+++ script.js
@@ -0,0 +1,46 @@
+// let a = +prompt('Write some number');
+// getArea(a);
+// function getArea(a){
+//     if (isNaN(a)){ 
+//         console.log('Введіть будь ласка число');
+//     }else if(a == ''){
+//         console.log('Введіть будь ласка радіус');
+//     }else{
+//         let b = Math.pow(a,2)*Math.PI;
+//     console.log("Площа круга = "+b);
+//     }
+// }
+
+// zavdannia 2
+
+// let a = +prompt('Write some number');
+// getSqrt(a);
+// function getSqrt(){
+//     if (isNaN(a)){ 
+//         console.log('Повинно бути числове значення');
+//     }else if(a == ''){
+//         console.log('Будь ласка, введіть число!');
+//     }else if(a<=0){
+//         console.log("Введіть додатнє число");
+//     }else{
+//         let b = Math.sqrt(a);
+//         console.log(`Корінь квадратний з ${a} = `+b);
+//     }
+// }
+
+// zavdannia 3
+
+// function pow() {
+//     let e = prompt('Write your furst numbers');
+// }
+
+// let MyMath = {
+//     PI : 3.14159,
+//     pow : pow
+// };
+
+// console.log("PI:",MyMath.PI);
+// console.log("POW:",MyMath.pow);
+
+
+
